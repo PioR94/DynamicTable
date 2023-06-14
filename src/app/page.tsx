0032e47b8b10d3eb7f22a1d2e154a91547fde1f6
PioR <roondi94@gmail.com>
2023-06-14 20:18:10 +0200

    add primereact and prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.min.css';
+import { Table } from '../components/Table/Table';
+
 export default function Page() {
-    return <></>
+  return <Table />;
 }
