8d9ed0a44ebfc429a0afa5529df98c61a706e2c5
PioR <roondi94@gmail.com>
2023-06-15 19:14:12 +0200

    add types

---
+++ src/types/book.ts
@@ -0,0 +1,12 @@
+export interface Book {
+  id: string;
+  kind: string;
+  title: string;
+  author: string;
+  href: string;
+  epoch: string;
+  genres: string;
+  epub: string;
+  mobi: string;
+  pdf: string;
+}
