e90d31b6eac99f9bde648fe4365d0890ac329b5d
PioR <roondi94@gmail.com>
2023-06-17 19:40:21 +0200

    prime react icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
 import { Table } from '../components/Table/Table';
 
 export default function Page() {
--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -9,23 +9,11 @@ import { Tag } from 'primereact/tag';
 import { Book } from '../../types/book';
 import { Author } from '../../types/author';
 import { BreadCrumb } from 'primereact/breadcrumb';
+import { MenuItem } from 'primereact/menuitem';
 
 export const Table = () => {
   const [authors, setAuthors] = useState<Author[]>([]);
   const [expandedRows, setExpandedRows] = useState<DataTableExpandedRows | DataTableValueArray | undefined>(undefined);
-  const [selectedProduct, setSelectedProduct] = useState<Author | null>(null);
-
-  // useEffect(() => {
-  //   const getBooks = async () => {
-  //     try {
-  //       const booksData = await getBooksData('adam-mickiewicz');
-  //       setBooks(booksData);
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
-  //   getBooks();
-  // }, []);
 
   useEffect(() => {
     const getAuthors = async () => {
@@ -52,25 +40,9 @@ export const Table = () => {
     setExpandedRows(undefined);
   };
 
-  const searchBodyTemplate = () => {
-    return <Button icon="pi pi-search" />;
-  };
-
   const imageBodyTemplate = (rowData: Book) => {
     return <img src={rowData.simple_thumb} alt={rowData.simple_thumb} width="64px" className="shadow-4" />;
   };
-
-  // const priceBodyTemplate = (rowData: Product) => {
-  //   return formatCurrency(rowData.price);
-  // };
-
-  // const ratingBodyTemplate = (rowData: Product) => {
-  //   return <Rating value={rowData.rating} readOnly cancel={false} />;
-  // };
-
-  // const statusBodyTemplate = (rowData: Product) => {
-  //   return <Tag value={rowData.inventoryStatus} severity={getProductSeverity(rowData)}></Tag>;
-  // };
 
   const allowExpansion = (rowData: Author) => {
     return rowData.name!.length > 0;
@@ -85,15 +57,18 @@ export const Table = () => {
           <Column header="Image" body={imageBodyTemplate} />
           <Column field="kind" header="Kind" />
           <Column field="epoch" header="Epoch" />
-          <Column field="amount" header="Amount" body={'amountBodyTemplate'} />
-          <Column field="status" header="Status" body={'statusOrderBodyTemplate'} />
+          <Column field="pdf" header="PDF" body={'amountBodyTemplate'} />
         </DataTable>
       </div>
     );
   };
 
+  const items: MenuItem[] = [{ label: 'Computer' }, { label: 'Notebook' }, { label: 'Accessories' }, { label: 'Backpacks' }, { label: 'Item' }];
+  const home: MenuItem = { icon: 'pi pi-home', url: 'https://primereact.org' };
+
   const header = (
     <div className="flex flex-wrap justify-content-end gap-2">
+      <BreadCrumb model={items} home={home} />
       <Button icon="pi pi-plus" label="Expand All" onClick={expandAll} text />
       <Button icon="pi pi-minus" label="Collapse All" onClick={collapseAll} text />
     </div>
@@ -115,11 +90,6 @@ export const Table = () => {
       >
         <Column expander={allowExpansion} style={{ width: '5rem' }} />
         <Column field="name" header="Author" sortable />
-
-        {/*<Column field="price" header="Price" sortable body={''} />*/}
-        {/*<Column field="category" header="Category" sortable />*/}
-        {/*<Column field="rating" header="Reviews" sortable body={'ratingBodyTemplate'} />*/}
-        {/*<Column field="inventoryStatus" header="Status" sortable body={'statusBodyTemplate'} />*/}
       </DataTable>
     </div>
   );
